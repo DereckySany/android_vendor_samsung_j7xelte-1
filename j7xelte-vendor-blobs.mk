8c91d683cff1a936ade6f2698e4f6a6fa6aead06
OVES786 <30339311+oves786@users.noreply.github.com>
2018-02-06 18:07:06 +0530

    Stock McDriverDaemon

--- j7xelte-vendor-blobs.mk
+++ j7xelte-vendor-blobs.mk
@@ -46,6 +46,7 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/j7xelte/proprietary/vendor/lib/libHpr_LocFace_mvfd_v1.0.so:system/vendor/lib/libHpr_LocFace_mvfd_v1.0.so \
     vendor/samsung/j7xelte/proprietary/vendor/lib/mediadrm/libdrmclearkeyplugin.so:system/vendor/lib/mediadrm/libdrmclearkeyplugin.so \
     vendor/samsung/j7xelte/proprietary/bin/gpsd:system/bin/gpsd \
+    vendor/samsung/j7xelte/proprietary/bin/mcDriverDaemon:system/bin/mcDriverDaemon \
     vendor/samsung/j7xelte/proprietary/etc/ca.pem:system/etc/ca.pem \
     vendor/samsung/j7xelte/proprietary/lib/hw/gps.default.so:system/lib/hw/gps.default.so \
     vendor/samsung/j7xelte/proprietary/lib/libfloatingfeature.so:system/lib/libfloatingfeature.so \

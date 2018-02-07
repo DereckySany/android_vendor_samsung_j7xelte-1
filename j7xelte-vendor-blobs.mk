6cf524e414a1a1d06c1b32d32861f3d26d739112
OVES786 <30339311+oves786@users.noreply.github.com>
2018-02-07 08:56:50 +0530

    Update Somx Dependencies

--- j7xelte-vendor-blobs.mk
+++ j7xelte-vendor-blobs.mk
@@ -48,6 +48,7 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/j7xelte/proprietary/bin/gpsd:system/bin/gpsd \
     vendor/samsung/j7xelte/proprietary/bin/mcDriverDaemon:system/bin/mcDriverDaemon \
     vendor/samsung/j7xelte/proprietary/etc/ca.pem:system/etc/ca.pem \
+    vendor/samsung/j7xelte/proprietary/etc/somxreg.conf:system/etc/somxreg.conf \
     vendor/samsung/j7xelte/proprietary/lib/hw/gps.default.so:system/lib/hw/gps.default.so \
     vendor/samsung/j7xelte/proprietary/lib/libfloatingfeature.so:system/lib/libfloatingfeature.so \
     vendor/samsung/j7xelte/proprietary/lib/libwrappergps.so:system/lib/libwrappergps.so \
@@ -92,6 +93,40 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/j7xelte/proprietary/lib/omx/libOMX.Exynos.VP8.Decoder.so:system/lib/omx/libOMX.Exynos.VP8.Decoder.so \
     vendor/samsung/j7xelte/proprietary/lib/omx/libOMX.Exynos.VP8.Encoder.so:system/lib/omx/libOMX.Exynos.VP8.Encoder.so \
     vendor/samsung/j7xelte/proprietary/lib/omx/libOMX.Exynos.WMV.Decoder.so:system/lib/omx/libOMX.Exynos.WMV.Decoder.so \
+    vendor/samsung/j7xelte/proprietary/lib/libExynosOMX_Core.so:system/lib/libExynosOMX_Core.so \
+    vendor/samsung/j7xelte/proprietary/lib/libExynosOMX_Resourcemanager.so:system/lib/libExynosOMX_Resourcemanager.so \
+    vendor/samsung/j7xelte/proprietary/lib/libsomx263dsw.so:system/lib/libsomx263dsw.so \
+    vendor/samsung/j7xelte/proprietary/lib/libsomx264dsw.so:system/lib/libsomx264dsw.so \
+    vendor/samsung/j7xelte/proprietary/lib/libsomxaacd.so:system/lib/libsomxaacd.so \
+    vendor/samsung/j7xelte/proprietary/lib/libsomxaace.so:system/lib/libsomxaace.so \
+    vendor/samsung/j7xelte/proprietary/lib/libsomxadpcmd.so:system/lib/libsomxadpcmd.so \
+    vendor/samsung/j7xelte/proprietary/lib/libsomxamrd.so:system/lib/libsomxamrd.so \
+    vendor/samsung/j7xelte/proprietary/lib/libsomxevrcd.so:system/lib/libsomxevrcd.so \
+    vendor/samsung/j7xelte/proprietary/lib/libsomxevrce.so:system/lib/libsomxevrce.so \
+    vendor/samsung/j7xelte/proprietary/lib/libsomxflacd.so:system/lib/libsomxflacd.so \
+    vendor/samsung/j7xelte/proprietary/lib/libsomxhevcdsw.so:system/lib/libsomxhevcdsw.so \
+    vendor/samsung/j7xelte/proprietary/lib/libsomxmp3d.so:system/lib/libsomxmp3d.so \
+    vendor/samsung/j7xelte/proprietary/lib/libsomxmp43d.so:system/lib/libsomxmp43d.so \
+    vendor/samsung/j7xelte/proprietary/lib/libsomxmp4vdsw.so:system/lib/libsomxmp4vdsw.so \
+    vendor/samsung/j7xelte/proprietary/lib/libsomxnaace.so:system/lib/libsomxnaace.so \
+    vendor/samsung/j7xelte/proprietary/lib/libsomxqcelpd.so:system/lib/libsomxqcelpd.so \
+    vendor/samsung/j7xelte/proprietary/lib/libsomxqcelpe.so:system/lib/libsomxqcelpe.so \
+    vendor/samsung/j7xelte/proprietary/lib/libsomxsr263d.so:system/lib/libsomxsr263d.so \
+    vendor/samsung/j7xelte/proprietary/lib/libsomxvc1dsw.so:system/lib/libsomxvc1dsw.so \
+    vendor/samsung/j7xelte/proprietary/lib/libsomxvp8d.so:system/lib/libsomxvp8d.so \
+    vendor/samsung/j7xelte/proprietary/lib/libsomxwmad.so:system/lib/libsomxwmad.so \
+    vendor/samsung/j7xelte/proprietary/lib/libsomxwmv7d.so:system/lib/libsomxwmv7d.so \
+    vendor/samsung/j7xelte/proprietary/lib/libsomxwmv8d.so:system/lib/libsomxwmv8d.so \
+    vendor/samsung/j7xelte/proprietary/lib/libarac.so:system/lib/libarac.so \
+    vendor/samsung/j7xelte/proprietary/lib/libsavsac.so:system/lib/libsavsac.so \
+    vendor/samsung/j7xelte/proprietary/lib/libsavscmn.so:system/lib/libsavscmn.so \
+    vendor/samsung/j7xelte/proprietary/lib/libsavsff.so:system/lib/libsavsff.so \
+    vendor/samsung/j7xelte/proprietary/lib/libsavsmeta.so:system/lib/libsavsmeta.so \
+    vendor/samsung/j7xelte/proprietary/lib/libsavsvc.so:system/lib/libsavsvc.so \
+    vendor/samsung/j7xelte/proprietary/lib/libsnaace.so:system/lib/libsnaace.so \
+    vendor/samsung/j7xelte/proprietary/lib/libsnamrnb.so:system/lib/libsnamrnb.so \
+    vendor/samsung/j7xelte/proprietary/lib/libsnamrwb.so:system/lib/libsnamrwb.so \
+    vendor/samsung/j7xelte/proprietary/lib/libsxqk.so:system/lib/libsxqk.so \
     vendor/samsung/j7xelte/proprietary/lib/libsec-ril.so:system/lib/libsec-ril.so \
     vendor/samsung/j7xelte/proprietary/lib/libsecril-client.so:system/lib/libsecril-client.so \
     vendor/samsung/j7xelte/proprietary/lib/libaudio-ril.so:system/lib/libaudio-ril.so \
